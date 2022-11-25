6337629dddd51ee7863d6acfacad10363800c005
NAYANA S <102555432+nayana005@users.noreply.github.com>
2022-11-25 15:43:19 +0530

    FundooApp

--- src/Components/TakeNoteThree/TakeNoteThree.jsx
+++ src/Components/TakeNoteThree/TakeNoteThree.jsx
@@ -1,10 +1,12 @@
 import React, { useState }from 'react'
 import './TakeNoteThree.css';
 import { grey } from '@mui/material/colors';
+import InputBase from '@mui/material/InputBase';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import AddAlertOutlinedIcon from '@mui/icons-material/AddAlertOutlined';
 import PersonAddAltOutlinedIcon from '@mui/icons-material/PersonAddAltOutlined';
 import PaletteOutlinedIcon from '@mui/icons-material/PaletteOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import ArchiveOutlinedIcon from '@mui/icons-material/ArchiveOutlined';
 import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
 import ColorPopper from '../ColorPopper/ColorPopper';
@@ -12,7 +14,12 @@ import ColorPopper from '../ColorPopper/ColorPopper';
 import { archiveNotes } from '../../services/dataService';
 import { deleteNotes } from '../../services/dataService';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import UndoOutlinedIcon from '@mui/icons-material/UndoOutlined';
+import RedoOutlinedIcon from '@mui/icons-material/RedoOutlined';
+import Button from '@mui/material/Button';
+import { updateNotes } from '../../services/dataService';
 // import { Paper } from '@mui/material';
 
 function TakeNoteThree(props) {
@@ -45,6 +52,23 @@ function TakeNoteThree(props) {
             console.log(response);
             props.autoRefreshing()
         }).catch((error) => console.log(error))
+    }
+
+    const takingTitle = (event) => {
+        setUserInfo(prevState => ({...prevState, title: event.target.value}))
+    }
+
+    const takingDescription = (event) => {
+        setUserInfo(prevState => ({...prevState, description: event.target.value}))
+    }
+
+    const saveUpdates = () => {
+        updateNotes(userInfo).then((response) => {
+            console.log(response)
+            props.autoRefreshing()
+        }).catch((error) => { console.log(error) })
+        setOpen(false);
+        //props.autoRefreshing()
     }
 
     return (
@@ -86,6 +110,53 @@ function TakeNoteThree(props) {
                 </Box>
             </Box>
             
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box>
+                    <Box className="MainContainerTwo" style={{backgroundColor: props.note.color, top: 200}}>
+                        <Box className="TitleRowTwo">
+                            <Box className="TitleTextTwo">
+                                <InputBase id="filled-search" 
+                                //placeholder="Title" 
+                                onChange={takingTitle} 
+                                defaultValue={userInfo.title}
+                                />
+                            </Box>
+                            <Box className="PinIconTwo">
+                                <PushPinOutlinedIcon fontSize='medium' sx={{ color: grey[600] }} />
+                            </Box>
+                        </Box>
+                        <Box className="TakeNoteTextTwo">
+                            <InputBase id="filled-search" 
+                            //placeholder="Take a note..." 
+                            onChange={takingDescription} 
+                            defaultValue={userInfo.description}
+                            />
+                        </Box>
+                        <Box className="TakeNoteIconsTwo">
+                            <Box className="IconsTwo">
+                                <AddAlertOutlinedIcon fontSize='small' sx={{ color: grey[800] }} />
+                                <PersonAddAltOutlinedIcon fontSize='small' sx={{ color: grey[800] }} />
+                                <PaletteOutlinedIcon fontSize='small' sx={{ color: grey[800] }} />
+                                <ImageOutlinedIcon fontSize='small' sx={{ color: grey[800] }} />
+                                <ArchiveOutlinedIcon fontSize='small' sx={{ color: grey[800] }}/>
+                                <MoreVertOutlinedIcon fontSize='small' sx={{ color: grey[800] }} />
+                                <UndoOutlinedIcon fontSize='small' sx={{ color: grey[600] }} />
+                                <RedoOutlinedIcon fontSize='small' sx={{ color: grey[600] }} />
+                            </Box>
+                            <Box className="CloseText">
+                                <Button variant="text" 
+                                onClick={saveUpdates}
+                                >close</Button>
+                            </Box>
+                        </Box>
+                    </Box>
+                </Box>
+            </Modal>
         </Box>
     )
 }
